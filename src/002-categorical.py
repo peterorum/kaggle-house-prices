1597f0c7f425e5d99a465af84adf79c9814912b8
Peter Orum <peter@peterorum.com>
2019-09-25 16:23:46 +1000

    custom features

--- src/002-categorical.py
+++ src/002-categorical.py
@@ -1,6 +1,6 @@
-# encode
-# local score 0.132
-# kaggle score 0.142
+# deal with categories
+# local score 0.126
+# kaggle score 0.1363
 # minimize score
 
 import csv
@@ -36,6 +36,8 @@ def timer():
 
     last_time = time()
 
+# run the model
+
 
 def evaluate(train, test, unique_id, target):
 
@@ -43,13 +45,13 @@ def evaluate(train, test, unique_id, target):
 
     model = XGBRegressor(random_state=0)
 
-    x_train, x_test, y_train, y_test = train_test_split(train.drop(
+    x_train, x_validate, y_train, y_validate = train_test_split(train.drop(
         [unique_id, target], axis=1), train[target], test_size=0.2, random_state=1)
 
     model.fit(x_train, y_train)
 
-    train_predictions = model.predict(x_test)
-    train_score = np.sqrt(mean_squared_error(np.log(train_predictions), np.log(y_test)))
+    train_predictions = model.predict(x_validate)
+    train_score = np.sqrt(mean_squared_error(np.log(train_predictions), np.log(y_validate)))
 
     test_predictions = model.predict(test[x_train.columns])
 
@@ -68,6 +70,29 @@ def remove_keys(list, keys):
     return result
 
 
+# clear empty numeric values that should not get a mean
+
+
+def clear_missing_values(train, test, columns, value):
+
+    for col in columns:
+        train[col] = train[col].fillna(value)
+        test[col] = test[col].fillna(value)
+
+    return train, test
+
+
+# convert numeric columns which are actually just categories
+
+
+def convert_numeric_categories(train, test, columns):
+
+    for col in columns:
+        train[col] = train[col].apply(str)
+        test[col] = test[col].apply(str)
+
+    return train, test
+
 # --- replace missing values
 
 
@@ -75,22 +100,24 @@ def replace_missing_values(train, test, unique_id, target):
 
     print(f'replace_missing_values')
 
-    # use mean for numerics, mode for categorical
+    # print(f'columns with missing data {train.columns[train.isna().any()]}')
+
+    # use mean for numerics, mode for categoric
 
     numeric_cols = [col for col in train.columns
                     if (train[col].dtype == 'int64') | (train[col].dtype == 'float64')]
 
     numeric_cols = remove_keys(numeric_cols, [unique_id, target])
 
-    categorical_cols = [col for col in train.columns if train[col].dtype == 'object']
-    categorical_cols = remove_keys(categorical_cols, [unique_id, target])
+    categoric_cols = [col for col in train.columns if train[col].dtype == 'object']
+    categoric_cols = remove_keys(categoric_cols, [unique_id, target])
 
     # replace missing numericals with mean
     for col in numeric_cols:
         if train[col].isna().any() | test[col].isna().any():
             mean = train[col].mean()
 
-            print(f'{col} mean {mean}')
+            # print(f'{col} mean {mean}')
 
             train[col].fillna(mean, inplace=True)
 
@@ -98,7 +125,7 @@ def replace_missing_values(train, test, unique_id, target):
                 test[col].fillna(mean, inplace=True)
 
     # convert to lowercase
-    for col in categorical_cols:
+    for col in categoric_cols:
         train[col] = train[col].apply(lambda x: str(x).lower())
 
         if col in test.columns:
@@ -108,12 +135,12 @@ def replace_missing_values(train, test, unique_id, target):
     train.replace('nan', np.nan, inplace=True)
     test.replace('nan', np.nan, inplace=True)
 
-    # replace missing categoricals with mode
-    for col in categorical_cols:
+    # replace missing categorics with mode
+    for col in categoric_cols:
         if train[col].isna().any() or test[col].isna().any():
             mode = train[col].mode()[0]
 
-            print(f'{col} mode {mean}')
+            # print(f'{col} mode {mode}')
 
             train[col].fillna(mode, inplace=True)
 
@@ -124,51 +151,51 @@ def replace_missing_values(train, test, unique_id, target):
 
     return train, test
 
-# --- categorical data
+# --- categoric data
 
 
-def encode_categorical_data(train, test, unique_id, target):
+def encode_categoric_data(train, test, unique_id, target):
 
-    print(f'get_categorical_data')
+    print(f'get_categoric_data')
 
     train_targets = train[target]
 
-    categorical_cols = [col for col in train.columns if train[col].dtype == 'object']
+    categoric_cols = [col for col in train.columns if train[col].dtype == 'object']
 
-    if unique_id in categorical_cols:
-        categorical_cols.remove(unique_id)
+    if unique_id in categoric_cols:
+        categoric_cols.remove(unique_id)
 
     # drop if too many values - usually a unique id column
 
     max_categories = train.shape[0] * 0.5
 
-    too_many_value_categorical_cols = [col for col in categorical_cols
-                                       if train[col].nunique() >= max_categories]
+    too_many_value_categoric_cols = [col for col in categoric_cols
+                                     if train[col].nunique() >= max_categories]
 
-    if len(too_many_value_categorical_cols) > 0:
-        print('dropping as too many categorical values', too_many_value_categorical_cols)
+    if len(too_many_value_categoric_cols) > 0:
+        print('dropping as too many categoric values', too_many_value_categoric_cols)
 
-    categorical_cols = [i for i in categorical_cols if i not in too_many_value_categorical_cols]
+    categoric_cols = [i for i in categoric_cols if i not in too_many_value_categoric_cols]
 
-    train = train.drop(too_many_value_categorical_cols, axis=1)
-    test.drop([col for col in too_many_value_categorical_cols
+    train = train.drop(too_many_value_categoric_cols, axis=1)
+    test.drop([col for col in too_many_value_categoric_cols
                if col in test.columns], axis=1, inplace=True)
 
     # one-hot encode if not too many values
 
     max_ohe_categories = 15
 
-    ohe_categorical_cols = [col for col in categorical_cols
-                            if train[col].nunique() <= max_ohe_categories]
+    ohe_categoric_cols = [col for col in categoric_cols
+                          if train[col].nunique() <= max_ohe_categories]
 
-    categorical_cols = [i for i in categorical_cols if i not in ohe_categorical_cols]
+    categoric_cols = [i for i in categoric_cols if i not in ohe_categoric_cols]
 
-    if len(ohe_categorical_cols) > 0:
-        print('one-hot encode', ohe_categorical_cols)
+    if len(ohe_categoric_cols) > 0:
+        # print('one-hot encode', ohe_categoric_cols)
 
         # one-hot encode & align to have same columns
-        train = pd.get_dummies(train, columns=ohe_categorical_cols)
-        test = pd.get_dummies(test, columns=ohe_categorical_cols)
+        train = pd.get_dummies(train, columns=ohe_categoric_cols)
+        test = pd.get_dummies(test, columns=ohe_categoric_cols)
         train, test = train.align(test, join='inner', axis=1)
 
         # restore after align
@@ -178,15 +205,35 @@ def encode_categorical_data(train, test, unique_id, target):
 
     # label encode the remainder (convert to integer)
 
-    label_encode_categorical_cols = categorical_cols
+    label_encode_categoric_cols = categoric_cols
 
-    print('label encode', label_encode_categorical_cols)
+    # print('label encode', label_encode_categoric_cols)
 
-    for col in label_encode_categorical_cols:
+    for col in label_encode_categoric_cols:
         lbl = LabelEncoder()
         lbl.fit(list(train[col].values.astype('str')) + list(test[col].values.astype('str')))
         train[col] = lbl.transform(list(train[col].values.astype('str')))
         test[col] = lbl.transform(list(test[col].values.astype('str')))
+
+    timer()
+
+    return train, test
+
+
+# custom features
+
+def add_custom_features(train, test, unique_id, target):
+
+    print(f'add_custom_features')
+
+    for df in [train, test]:
+        df['TotalSF'] = df['TotalBsmtSF'] + df['1stFlrSF'] + df['2ndFlrSF']
+
+        df['Total_sqr_footage'] = (df['BsmtFinSF1'] + df['BsmtFinSF2'] +
+                                   df['1stFlrSF'] + df['2ndFlrSF'])
+
+        df['Total_Bathrooms'] = (df['FullBath'] + (0.5 * df['HalfBath']) +
+                                 df['BsmtFullBath'] + (0.5 * df['BsmtHalfBath']))
 
     timer()
 
@@ -207,9 +254,18 @@ def run():
 
     # ----------
 
+    train, test = clear_missing_values(train, test, ['GarageYrBlt', 'GarageArea', 'GarageCars'], 0)
+    train, test = clear_missing_values(
+        train, test, ['GarageType', 'GarageFinish', 'GarageQual', 'GarageCond', 'FireplaceQu', 'Alley', 'BsmtQual',
+                      'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2', 'FireplaceQu', 'PoolQC', 'Fence', 'MiscFeature'], 'NA')
+
+    train, test = convert_numeric_categories(train, test, ['MSSubClass'])
+
     train, test = replace_missing_values(train, test, unique_id, target)
 
-    train, test = encode_categorical_data(train, test, unique_id, target)
+    train, test = add_custom_features(train, test, unique_id, target)
+
+    train, test = encode_categoric_data(train, test, unique_id, target)
 
     # ----------
 
