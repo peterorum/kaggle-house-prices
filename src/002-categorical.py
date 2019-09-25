6ef4af08011f5016b6fdd1281159dd29fb419cfe
Peter Orum <peter@peterorum.com>
2019-09-25 14:27:43 +1000

    encode

--- src/002-categorical.py
+++ src/002-categorical.py
@@ -1,19 +1,29 @@
-# train/test split
-# local score  0.132
-# kaggle score 0.141
+# encode
+# local score 0.132
+# kaggle score 0.142
 # minimize score
 
-import pandas as pd
-import numpy as np
-from time import time
+import csv
+import os
 import sys  # noqa
-
-from sklearn.model_selection import train_test_split
-from xgboost import XGBRegressor
+from time import time
+from pprint import pprint  # noqa
+import numpy as np
+import pandas as pd
 from sklearn.metrics import mean_squared_error
-from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+from xgboost import XGBRegressor
 
+pd.options.display.float_format = '{:.4f}'.format
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 2000)
+np.set_printoptions(threshold=sys.maxsize)
+
+is_kaggle = os.environ['HOME'] == '/tmp'
+
+zipext = ''  # if is_kaggle else '.zip'
+train_file = 'train'  # if is_kaggle else 'sample'
 
 start_time = time()
 last_time = time()
@@ -22,102 +32,196 @@ last_time = time()
 def timer():
     global last_time
 
-    print(f'{((time() - last_time) / 60):.1f} mins\n')  # noqa
+    print(f'{((time() - last_time) / 60):.1f}, {((time() - start_time) / 60):.1f} mins\n')  # noqa
 
     last_time = time()
 
+
+def evaluate(train, test, unique_id, target):
+
+    print('evaluate')
+
+    model = XGBRegressor(random_state=0)
+
+    x_train, x_test, y_train, y_test = train_test_split(train.drop(
+        [unique_id, target], axis=1), train[target], test_size=0.2, random_state=1)
+
+    model.fit(x_train, y_train)
+
+    train_predictions = model.predict(x_test)
+    train_score = np.sqrt(mean_squared_error(np.log(train_predictions), np.log(y_test)))
+
+    test_predictions = model.predict(test[x_train.columns])
+
+    timer()
+
+    return test_predictions, train_score
+
+
+# --- remove keys
+
+
+def remove_keys(list, keys):
+
+    result = [x for x in list if x not in keys]
+
+    return result
+
+
+# --- replace missing values
+
+
+def replace_missing_values(train, test, unique_id, target):
+
+    print(f'replace_missing_values')
+
+    # use mean for numerics, mode for categorical
+
+    numeric_cols = [col for col in train.columns
+                    if (train[col].dtype == 'int64') | (train[col].dtype == 'float64')]
+
+    numeric_cols = remove_keys(numeric_cols, [unique_id, target])
+
+    categorical_cols = [col for col in train.columns if train[col].dtype == 'object']
+    categorical_cols = remove_keys(categorical_cols, [unique_id, target])
+
+    # replace missing numericals with mean
+    for col in numeric_cols:
+        if train[col].isna().any() | test[col].isna().any():
+            mean = train[col].mean()
+
+            print(f'{col} mean {mean}')
+
+            train[col].fillna(mean, inplace=True)
+
+            if col in test.columns:
+                test[col].fillna(mean, inplace=True)
+
+    # convert to lowercase
+    for col in categorical_cols:
+        train[col] = train[col].apply(lambda x: str(x).lower())
+
+        if col in test.columns:
+            test[col] = test[col].apply(lambda x: str(x).lower())
+
+    # replace string nan with np.nan
+    train.replace('nan', np.nan, inplace=True)
+    test.replace('nan', np.nan, inplace=True)
+
+    # replace missing categoricals with mode
+    for col in categorical_cols:
+        if train[col].isna().any() or test[col].isna().any():
+            mode = train[col].mode()[0]
+
+            print(f'{col} mode {mean}')
+
+            train[col].fillna(mode, inplace=True)
+
+            if col in test.columns:
+                test[col].fillna(mode, inplace=True)
+
+    timer()
+
+    return train, test
+
+# --- categorical data
+
+
+def encode_categorical_data(train, test, unique_id, target):
+
+    print(f'get_categorical_data')
+
+    train_targets = train[target]
+
+    categorical_cols = [col for col in train.columns if train[col].dtype == 'object']
+
+    if unique_id in categorical_cols:
+        categorical_cols.remove(unique_id)
+
+    # drop if too many values - usually a unique id column
+
+    max_categories = train.shape[0] * 0.5
+
+    too_many_value_categorical_cols = [col for col in categorical_cols
+                                       if train[col].nunique() >= max_categories]
+
+    if len(too_many_value_categorical_cols) > 0:
+        print('dropping as too many categorical values', too_many_value_categorical_cols)
+
+    categorical_cols = [i for i in categorical_cols if i not in too_many_value_categorical_cols]
+
+    train = train.drop(too_many_value_categorical_cols, axis=1)
+    test.drop([col for col in too_many_value_categorical_cols
+               if col in test.columns], axis=1, inplace=True)
+
+    # one-hot encode if not too many values
+
+    max_ohe_categories = 15
+
+    ohe_categorical_cols = [col for col in categorical_cols
+                            if train[col].nunique() <= max_ohe_categories]
+
+    categorical_cols = [i for i in categorical_cols if i not in ohe_categorical_cols]
+
+    if len(ohe_categorical_cols) > 0:
+        print('one-hot encode', ohe_categorical_cols)
+
+        # one-hot encode & align to have same columns
+        train = pd.get_dummies(train, columns=ohe_categorical_cols)
+        test = pd.get_dummies(test, columns=ohe_categorical_cols)
+        train, test = train.align(test, join='inner', axis=1)
+
+        # restore after align
+        train[target] = train_targets
+
+    # possibly rank encode rather than ohe. see gstore.
+
+    # label encode the remainder (convert to integer)
+
+    label_encode_categorical_cols = categorical_cols
+
+    print('label encode', label_encode_categorical_cols)
+
+    for col in label_encode_categorical_cols:
+        lbl = LabelEncoder()
+        lbl.fit(list(train[col].values.astype('str')) + list(test[col].values.astype('str')))
+        train[col] = lbl.transform(list(train[col].values.astype('str')))
+        test[col] = lbl.transform(list(test[col].values.astype('str')))
+
+    timer()
+
+    return train, test
 
 # --------------------- run
 
 
 def run():
 
+    unique_id = 'Id'
     target = 'SalePrice'
 
-    # Read the data
-    X_train_full = pd.read_csv('../input/train.csv', index_col='Id')
-    X_test_full = pd.read_csv('../input/test.csv', index_col='Id')
+    # load data
 
-    # Obtain target and predictors
-    y = X_train_full[target]
+    train = pd.read_csv(f'../input/{train_file}.csv{zipext}')
+    test = pd.read_csv(f'../input/test.csv{zipext}')
 
-    numeric_cols = [col for col in X_train_full.columns
-                    if (X_train_full[col].dtype == 'int64') | (X_train_full[col].dtype == 'float64')]
+    # ----------
 
-    categorical_cols = [col for col in X_train_full.columns if X_train_full[col].dtype == 'object']
+    train, test = replace_missing_values(train, test, unique_id, target)
 
-    numeric_cols.remove(target)
+    train, test = encode_categorical_data(train, test, unique_id, target)
 
-    X_train_full_numeric = X_train_full[numeric_cols]
-    X_train_full_categorical = pd.DataFrame(X_train_full[categorical_cols])
-    X_test_full_numeric = X_test_full[numeric_cols]
-    X_test_full_categorical = pd.DataFrame(X_test_full[categorical_cols])
+    # ----------
 
-    # ----------------- missing values
+    test_predictions, train_score = evaluate(train, test, unique_id, target)
 
-    # impute missing values
+    print('score', train_score)
 
-    my_imputer = SimpleImputer()
+    test[target] = test_predictions
 
-    X_numeric = pd.DataFrame(my_imputer.fit_transform(X_train_full_numeric), columns=numeric_cols)
-    X_test_numeric = pd.DataFrame(my_imputer.transform(X_test_full_numeric), columns=numeric_cols)
+    predictions = test[[unique_id, target]]
 
-    # replace string nan with np.nan
-    X_train_full_categorical.replace('nan', np.nan, inplace=True)
-    X_test_full_categorical.replace('nan', np.nan, inplace=True)
-
-    # replace missing categoricals with mode
-    for col in categorical_cols:
-        if X_train_full_categorical[col].isna().any() or X_test_full_categorical[col].isna().any():
-            mode = X_train_full_categorical[col].mode()[0]
-
-            X_train_full_categorical[col].fillna(mode, inplace=True)
-
-            if col in X_test_full_categorical.columns:
-                X_test_full_categorical[col].fillna(mode, inplace=True)
-
-    # ---------------- categorical columns
-
-    label_encoder = LabelEncoder()
-
-    for col in categorical_cols:
-        X_train_full_categorical[col] = label_encoder.fit_transform(X_train_full_categorical[col])
-        X_test_full_categorical[col] = label_encoder.transform(X_test_full_categorical[col])
-
-    # ---------------- join back together
-
-    X = pd.concat([X_numeric, X_train_full_categorical], axis=1)
-
-    X_test = pd.concat([X_test_numeric, X_test_full_categorical], axis=1)
-
-    print(X)
-    print(y)
-
-    sys.exit(0)
-
-    # ---------------- split into training & validation
-    X_train, X_validate, y_train, y_validate = train_test_split(X, y, train_size=0.8, test_size=0.2,
-                                                                random_state=0)
-
-    my_model = XGBRegressor(random_state=0)
-
-    # --------------- fit the model to the training data & validate for score
-    my_model.fit(X_train, y_train)
-    preds_validate = my_model.predict(X_validate)
-    score = np.sqrt(mean_squared_error(np.log(y_validate), np.log(preds_validate)))
-
-    print(f'score: {score}')
-
-    # --------------- generate test predictions on full set
-    my_model.fit(X, y)
-
-    preds_test = my_model.predict(X_test)
-
-    # --------------- save predictions in format used for competition scoring
-    output = pd.DataFrame({'Id': X_test.index,
-                           target: preds_test})
-
-    output.to_csv('submission.csv', index=False)
+    predictions.to_csv('submission.csv', index=False)
 
 
 # -------- main
